021f99727ad094294aeaecd60c69b5752901bb1a
ABULBASAR36 <150958151+ABULBASAR36@users.noreply.github.com>
2023-12-05 15:15:02 +0600

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,31 @@
+// class Animal2 {
+//     void eat() {
+//         System.out.println("Eating...");
+//     }
+// }
+
+// class Dog extends Animal2 {
+//     void sound() {
+//         System.out.println("Barking...");
+//     }
+// }
+
+// class Cat extends Animal2 {
+//     void meow() {
+//         System.out.println("Meow...");
+//     }
+// }
+
+//  public class Animal {
+//     public static void main(String[] args) {
+//         Cat c = new Cat();
+//         c.meow();
+//         // c.sound(); // This won't work because sound() method is not part of Cat class
+//         c.eat();
+//     }
+// }
+public class Animal {
+    public static void main(String[] args) {
+        
+    }
+}
